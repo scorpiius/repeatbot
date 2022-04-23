947a873bcae7222c04f5ccb2cabbf073e7498148
V <scorpiius@yandex.ru>
2022-04-23 18:06:54 +0300

    commit

---
+++ RepeatBot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from time import sleep
+import telebot
+
+bot = telebot.TeleBot('5321282908:AAGuTGeVp-5cjHL_gZNaRvrDLNCnBFGZCco')
+
+
+@bot.message_handler()
+def get_user_text(message):
+	sleep(5)
+	bot.send_message(message.chat.id, message.text)
+
+bot.polling(none_stop=True)
